e03bf52cfcf676e477139bbd45bf59a2dfd3e5d7
ernest nnodu <ernest.nnodu@outlook.com>
2025-02-20 16:12:53 +0000

    Create file entity

---
+++ src/server/Entity/File.java
@@ -0,0 +1,17 @@
+package server.Entity;
+
+public class File {
+    String name;
+
+    public File(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
